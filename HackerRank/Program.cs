60381887757216bd72c77231926a2a19692443da
Gerson Azabache <gerson_dunk@hotmail.com>
2024-08-18 11:59:11 -0500

    plusminus algorithm added

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HackerRank;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hacker Rank");
+
+        plusMinus([1,2,3]);
+    }
+
+    public static void plusMinus(List<int> arr)
+    {
+        decimal positives = 0;
+        decimal negatives = 0;
+        decimal zeros = 0;
+
+        foreach (int item in arr)
+        {
+            if (item == 0)
+            {
+                zeros++;
+                continue;
+            }
+            else if (item > 0)
+                positives++;
+            else
+                negatives++;
+        }
+        Console.WriteLine(positives / arr.Count);
+        Console.WriteLine(negatives / arr.Count);
+        Console.WriteLine(zeros / arr.Count);
+    }
+}
